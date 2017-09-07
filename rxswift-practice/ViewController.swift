75690c07cf9effc5cdc698ad59f2bd6aa97de2f0
Kazumasa Shimomura <kazlv.1110@gmail.com>
2017-09-07 12:27:00 +0900

    rx textfield

--- rxswift-practice/ViewController.swift
+++ rxswift-practice/ViewController.swift
@@ -7,17 +7,33 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var field1: UITextField!
+    
+    let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setup()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setup() {
+        field1.rx.text
+            .map{"[\($0!)]"}
+            .bind(to: label1.rx.text)
+            .addDisposableTo(disposeBag)
     }
 
 
